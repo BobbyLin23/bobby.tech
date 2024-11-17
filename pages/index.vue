939b084fd5afc41943c008067863bda9b898a041
BobbyLin23 <linzhangsheng23@gmail.com>
2024-11-17 18:56:41 +0800

    chore: init project

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const val = ref(1)
+</script>
+
+<template>
+  <div class="p-2 bg-red-500 dark:bg-orange-600">
+    <UButton @click="val++">
+      Button
+    </UButton>
+    {{ val }}
+  </div>
+</template>
